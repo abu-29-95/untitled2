263151904ddcc59022003737af5fbd61eb30242e
Abylkhaiyr <abul_29_95@mail.eu>
2022-11-18 18:00:13 +0600

    Lesson7

--- src/test/java/org/example/Lesson6/AbstractTest.java
+++ src/test/java/org/example/Lesson6/AbstractTest.java
@@ -1,29 +1,33 @@
 package org.example.Lesson6;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.example.Lesson7.MyWebDriverEventListener;
 import org.jsoup.internal.ReturnsAreNonnullByDefault;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class AbstractTest {
-    static WebDriver driver;
+public abstract class AbstractTest {
+    static EventFiringWebDriver eventDriver;
 
     @BeforeAll
     static void driverConnection (){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maxinized");
+        options.addArguments("start-maximized");
         options.addArguments("--incognito");
 
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        eventDriver = new EventFiringWebDriver( new ChromeDriver(options));
+        eventDriver.register(new MyWebDriverEventListener());
+        eventDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
     }
 
     @BeforeEach
@@ -35,11 +39,26 @@ public class AbstractTest {
     }
 
     @AfterAll
-    static void close(){
-        //driver.quit();
+    public static void close(){
+        if(eventDriver !=null) eventDriver.quit();
+
     }
 
-    public static WebDriver getDriver() {
-        return driver;
+    @AfterEach
+    public void checkBrowser(){
+        List<LogEntry> allLogRows = getDriver().manage().logs().get(LogType.BROWSER).getAll();
+
+        if(!allLogRows.isEmpty()){
+            if(allLogRows.size()>0){
+                allLogRows.forEach(logEntry -> {
+                    System.out.println(logEntry.getMessage());
+                });
+            }
+
+        }
+    }
+
+    public WebDriver getDriver() {
+        return this.eventDriver;
     }
 }
